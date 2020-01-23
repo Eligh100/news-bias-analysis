b2505a41364338dbea42f339dea850f9bced1b62
Eligh John <eaj627@cs.bham.ac.uk>
2020-01-23 16:49:39 +0000

    Added comment

--- sentiment-processor/ArticlePreProcessor.py
+++ sentiment-processor/ArticlePreProcessor.py
@@ -1,3 +1,13 @@
+'''
+use VADeR?
+NLTK?
+Look into necessary steps for effective sentiment analysis
+Is training required? How can it help my system?
+Could TF-IDF be used to help identify newspaper views change over time?
+    Compare old articles with newer one
+    Interesante!
+'''
+
 class ArticlePreProcessor:
 
     def __init__(self):
