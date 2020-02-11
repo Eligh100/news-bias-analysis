a6311a7d50da5bf22aec6b3f8728ff09ae245005
Eligh John <eaj627@cs.bham.ac.uk>
2020-02-11 12:41:59 +0000

    Improving CorEx topic model

--- manifesto-scraper/TopicAnalyser.py
+++ manifesto-scraper/TopicAnalyser.py
@@ -5,13 +5,15 @@ Combine this with keyword extraction
 
 
 import os
-from sklearn.feature_extraction.text import TfidfVectorizer
+import scipy.sparse as ss
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer, WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 from sklearn.decomposition import NMF, LatentDirichletAllocation
 from corextopic import corextopic as ct
 import pandas as pd
+import numpy as np
 import re
 
 def display_topics(model, feature_names, no_top_words):
@@ -60,22 +62,26 @@ for manifesto in os.listdir('manifestos'):
         manifestoText.close()
 
 
-
-tfidf_vectorizer = TfidfVectorizer(min_df = 0.5, max_df = 10, ngram_range=(1,2))
-tfidf = tfidf_vectorizer.fit_transform(manifestoTexts)
-tfidf_feature_names = tfidf_vectorizer.get_feature_names()
+vectorizer = CountVectorizer(max_features=20000, binary=True)
+doc_word = vectorizer.fit_transform(manifestoTexts)
+doc_word = ss.csr_matrix(doc_word)
+print(doc_word.shape)
+words = list(np.asarray(vectorizer.get_feature_names()))
+#tfidf_vectorizer = TfidfVectorizer(min_df = 0.5, max_df = 10, ngram_range=(1,2))
+#tfidf = tfidf_vectorizer.fit_transform(manifestoTexts)
+#tfidf_feature_names = tfidf_vectorizer.get_feature_names()
 
 
 
 
 # Anchors designed to nudge the model towards measuring specific genres
 anchors = [
-    ["poverty inequality","playing field", "poor", "poorest", "poverty", "income tax"], # Poverty/Income Inequality
-    ["policing", "prison", "police force", "perpetrators", "racism", "police forces", "places worship", "abusers, ""crime", "knife", "criminals", "police"], # crime and policing
-    ["economy", "economics", "exchange rate", "pound", "tariff", "business", "powerhouse", "pounds", "privatisation", "private investment"], # business and economics
+    ["poverty inequality","playing field", "poor", "poorest", "poverty", "income tax", "tax"], # Poverty/Income Inequality
+    ["policing", "prison", "police force", "perpetrators", "racism", "police forces", "places worship", "abusers, ""crime", "knife", "criminals", "police", "terrorist", "terrorism"], # crime and policing
+    ["economy", "economics", "exchange rate", "pound", "tariff", "business", "powerhouse", "pounds", "privatisation", "private investment", "deficit", "gdp", "national debt"], # business and economics
     ["education", "schools", "school", "teaching", "teachers", "curriculum", "grammar school", "academic"], # education
     ["nhs", "national health", "hospitals", "nurses", "doctors", "medicine", "privatisation", "privatisation nhs"], # healthcare
-    ["brexit" "nodeal brexit", "no deal", "postbrexit", "brussels", "EU", "european union", "eu", "European Union"], # Brexit
+    ["brexit" "nodeal brexit", "no deal", "postbrexit", "brussels", "EU", "european union", "eu", "European Union", "sovereignty"], # Brexit
     ["climate change", "climate", "climate emergency", "environment", "fossil fuels", "carbon", "zero carbon", "zerocarbon", "planet", "plastic", "petrol diesel", "plastic waste", "pollution", "petrol", "public transport"], # environment and climate change
     #["housing", "houses", "social housing", "homes", "housebuilding", "housing supply", "affordable"], # housing
     ["pledge", "accessible", "powerful", "policies", "points", "powers", "ability", "politics", "practice", "premium", "prioritise", 
@@ -87,23 +93,36 @@ anchors = [
     "practical", "population", "pressing", "planned"] # FILTER TOPIC - FILLED WITH AMBIGUOUS WORDS
 ]
 
+docs = ["Brexit Party Manifesto", "Conservative Manifesto", "Green Manifesto", "Labour Manifesto", "Liberal Democrats Manifesto",
+        "Plaid Cymru Manifesto", "SNP Manifesto", "UKIP Manifesto"]
+
 anchors = [
-    [a for a in topic if a in tfidf_feature_names]
+    [a for a in topic if a in words]
     for topic in anchors
 ]
 
-model = ct.Corex(n_hidden=10)
+model = ct.Corex(
+    n_hidden=8,
+    words = words,
+)
 model = model.fit(
-    tfidf,
-    words=tfidf_feature_names,
+    doc_word,
+    words=words,
+    docs = docs,
     anchors=anchors,
-    anchor_strength=3 # Anchor weighting/reliance
+    anchor_strength=6 # Anchor weighting/reliance
 )
 
+
+#print(model.p_y_given_x)
+#print(model.get_top_docs(topic=0, n_docs=8, sort_by="log_prob"))
 
 for j, topic_ngrams in enumerate(model.get_topics(n_words=20)):
     topic_ngrams = [ngram[0] for ngram in topic_ngrams] #if ngram[1] > 0]
     print("Topic #{}: {}".format(j+1, ", ".join(topic_ngrams)))
+
+
+
 
 
 # USE COREX - SEMI-SUPERVISED LEARNING TECHNIQUE
